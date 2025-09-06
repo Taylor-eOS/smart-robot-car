a14d8d795bd40b8c9c51e9496e6163121ea85b8f
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-09-06 12:35:46 +0200

    Spin test
    Still on imperfect pins

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,16 @@
 #define LEDC_FREQ 5000
 #define LEDC_RES 8
 
-const int MIN_INNER_PWM = 20;
-const int BURST_PWM = 200;
-const int BURST_MS = 80;
 bool rightFlip = false;
-int curLeft = 0;
-int curRight = 0;
 
-int clampSigned(int v) {
-  if (v > 255) return 255;
-  if (v < -255) return -255;
-  return v;
-}
-
-void setDirectionPins(int leftDir, int rightDir) {
-  int l = leftDir;
-  int r = rightDir;
-  if (r != 0 && rightFlip) r = -r;
-  if (l > 0) { digitalWrite(L_IN1, LOW); digitalWrite(L_IN2, HIGH); }
-  else if (l < 0) { digitalWrite(L_IN1, HIGH); digitalWrite(L_IN2, LOW); }
-  else { digitalWrite(L_IN1, LOW); digitalWrite(L_IN2, LOW); }
-  if (r > 0) { digitalWrite(R_IN1, LOW); digitalWrite(R_IN2, HIGH); }
-  else if (r < 0) { digitalWrite(R_IN1, HIGH); digitalWrite(R_IN2, LOW); }
-  else { digitalWrite(R_IN1, LOW); digitalWrite(R_IN2, LOW); }
+void setDirectionRaw(bool isRight, int dir) {
+  int eff = dir;
+  if (isRight && rightFlip) eff = -eff;
+  uint8_t a = isRight ? R_IN1 : L_IN1;
+  uint8_t b = isRight ? R_IN2 : L_IN2;
+  if (eff > 0) { digitalWrite(a, LOW); digitalWrite(b, HIGH); }
+  else if (eff < 0) { digitalWrite(a, HIGH); digitalWrite(b, LOW); }
+  else { digitalWrite(a, LOW); digitalWrite(b, LOW); }
 }
 
 void attachAndZeroPwm() {
@@ -47,54 +34,30 @@ void attachAndZeroPwm() {
   ledcAttachPin(ENB_PIN, ENB_CH);
   ledcWrite(ENA_CH, 0);
   ledcWrite(ENB_CH, 0);
-  curLeft = 0;
-  curRight = 0;
 }
 
-void applyPwmBoth(uint8_t leftVal, uint8_t rightVal) {
+void applyPwmBoth(int leftVal, int rightVal) {
   ledcWrite(ENA_CH, leftVal);
   ledcWrite(ENB_CH, rightVal);
-  curLeft = leftVal;
-  curRight = rightVal;
 }
 
-void rampToTargetsWithBurst(int leftTarget, int rightTarget, int stepDelay = 8) {
-  leftTarget = clampSigned(leftTarget);
-  rightTarget = clampSigned(rightTarget);
-  int lSign = (leftTarget > 0) - (leftTarget < 0);
-  int rSign = (rightTarget > 0) - (rightTarget < 0);
-  int absL = abs(leftTarget);
-  int absR = abs(rightTarget);
-  if (absL == 0 && absR > 0) absL = min(MIN_INNER_PWM, absR);
-  if (absR == 0 && absL > 0) absR = min(MIN_INNER_PWM, absL);
-  setDirectionPins(lSign, rSign);
-  applyPwmBoth(0, 0);
-  int burstL = absL > 0 ? BURST_PWM : 0;
-  int burstR = absR > 0 ? BURST_PWM : 0;
-  if (burstL || burstR) {
-    applyPwmBoth(burstL, burstR);
-    delay(BURST_MS);
-  }
-  int l = min(burstL, absL);
-  int r = min(burstR, absR);
-  while (l != absL || r != absR) {
-    if (l < absL) l++;
-    else if (l > absL) l--;
-    if (r < absR) r++;
-    else if (r > absR) r--;
-    applyPwmBoth(l, r);
+void rampTo(int leftTarget, int rightTarget, int stepDelay = 6) {
+  int curL = ledcRead(ENA_CH);
+  int curR = ledcRead(ENB_CH);
+  while (curL != leftTarget || curR != rightTarget) {
+    if (curL < leftTarget) curL++;
+    else if (curL > leftTarget) curL--;
+    if (curR < rightTarget) curR++;
+    else if (curR > rightTarget) curR--;
+    applyPwmBoth(curL, curR);
     delay(stepDelay);
   }
 }
 
-void holdMs(unsigned long ms) {
-  unsigned long start = millis();
-  while (millis() - start < ms) delay(10);
-}
-
-void stopBothImmediate() {
+void stopBoth() {
   applyPwmBoth(0, 0);
-  setDirectionPins(0, 0);
+  setDirectionRaw(false, 0);
+  setDirectionRaw(true, 0);
 }
 
 void setup() {
@@ -104,9 +67,9 @@ void setup() {
   pinMode(R_IN2, OUTPUT);
   Serial.begin(115200);
   attachAndZeroPwm();
-  stopBothImmediate();
+  stopBoth();
+  Serial.println("Ready. Type 'i' within 3s to invert right motor.");
   unsigned long start = millis();
-  Serial.println("Ready. Type 'i' within 3s to invert right motor");
   while (millis() - start < 3000 && Serial.available() == 0) delay(10);
   if (Serial.available()) {
     char c = Serial.read();
@@ -115,12 +78,23 @@ void setup() {
 }
 
 void loop() {
-  rampToTargetsWithBurst(200, 120);
-  holdMs(1000);
-  rampToTargetsWithBurst(120, 200);
-  holdMs(1000);
-  rampToTargetsWithBurst(0, 0, 6);
-  stopBothImmediate();
-  delay(4000);
+  setDirectionRaw(false, 1);
+  setDirectionRaw(true, 1);
+  rampTo(200, 200);
+  delay(1000);
+  stopBoth();
+  delay(500);
+  setDirectionRaw(false, -1);
+  setDirectionRaw(true, 1);
+  rampTo(200, 200);
+  delay(1200);
+  stopBoth();
+  delay(800);
+  setDirectionRaw(false, 1);
+  setDirectionRaw(true, -1);
+  rampTo(200, 200);
+  delay(1200);
+  stopBoth();
+  delay(3000);
 }
 
